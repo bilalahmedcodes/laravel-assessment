ff6cace853587b203f202e9cb16728b00da737a6
Bilal Ahmed <bilalahmedcodes@gmail.com>
2023-12-16 01:44:55 +0500

    seedings of user done

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 class UserSeeder extends Seeder
 {
     /**
@@ -14,6 +16,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create([
+            'name' => 'Bilal Ahmed',
+            'email' => 'bilalahmedcodes@gmail.com',
+            'password' => Hash::make('test12345'),
+        ]);
+
+        $role = Role::where('name', 'Super Admin')->first();
+        $user->assignRole($role);
     }
 }
